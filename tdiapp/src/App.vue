03b349baaf598c9170745f761207103c366d2104
Afrosiyob <afrosiyob.a97@gmail.com>
2020-08-27 19:39:55 +0500

    add
    add

---
+++ tdiapp/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <router-link to="/">home</router-link>
+    <router-link to="/person">person</router-link>
+    <router-view></router-view>
+
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ tdiapp/src/views/Person.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    this is person
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Person",
+};
+</script>
+
+<style lang="scss" scoped></style>
